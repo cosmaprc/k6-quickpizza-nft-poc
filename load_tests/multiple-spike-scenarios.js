731a008bc8864a8dd355427acda9e2a5ed7aaecb
cosmaprc <cosmaprc@gmail.com>
2025-05-02 08:54:01 +0100

    Add missing cookies in multi scenario test

--- load_tests/multiple-spike-scenarios.js
+++ load_tests/multiple-spike-scenarios.js
@@ -2,6 +2,7 @@ import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 import { randomString } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { group } from "k6";
+import http from "k6/http";
 
 import { makePizza } from "../api/pizza.js";
 import {
@@ -34,37 +35,54 @@ export function handleSummary(data) {
 }
 
 export function setup() {
-  const USERNAME = `${randomString(10)}@example.com`;
-  const PASSWORD = "secret";
+  let username = `${randomString(10)}1@example.com`;
+  let password = "secret";
   let authToken = null; // This will be set after the user is created and logged in
   group("Users setup operations", () => {
-    createUser(`${BASE_URL}/api/users`, USERNAME, PASSWORD);
+    createUser(`${BASE_URL}/api/users`, username, password);
     authToken = loginUser(
       `${BASE_URL}/api/users/token/login`,
-      USERNAME,
-      PASSWORD,
+      username,
+      password,
     );
   });
-  return authToken;
+  // Subsequent requests need the cookies from the login request
+  const jar = http.cookieJar();
+  let cookies = jar.cookiesForURL(`${BASE_URL}/api/users/token/login`);
+  return {
+    authToken: authToken,
+    cookies: cookies,
+  };
 }
 
 export function createAndLoginUserScenario() {
-  const USERNAME = `${randomString(10)}@example.com`;
-  const PASSWORD = "secret";
+  let username = `${randomString(10)}2@example.com`;
+  let password = "secret";
   group("Users operations", () => {
-    createUser(`${BASE_URL}/api/users`, USERNAME, PASSWORD);
-    loginUser(`${BASE_URL}/api/users/token/login`, USERNAME, PASSWORD);
+    createUser(`${BASE_URL}/api/users`, username, password);
+    loginUser(`${BASE_URL}/api/users/token/login`, username, password);
   });
 }
 
-export function crudPizzaRatingScenario(authToken) {
-  const requestConfig = () => ({
+export function crudPizzaRatingScenario(setupData) {
+  let requestConfig = () => ({
     headers: {
-      Authorization: `Bearer ${authToken}`,
+      Authorization: `Bearer ${setupData.authToken}`,
     },
   });
-  let pizzaId = makePizza(`${BASE_URL}/api/pizza`, requestConfig());
-  const URL = `${BASE_URL}/api/ratings`;
+  let pizzaId = null; // This will be set after the pizza is created
+  group("Pizza operations", () => {
+    // The first call in the chain needs cookies from the setup function
+    const jar = http.cookieJar();
+    jar.set(`${BASE_URL}/api/pizza`, "AWSALB", setupData.cookies.AWSALB);
+    jar.set(
+      `${BASE_URL}/api/pizza`,
+      "AWSALBCORS",
+      setupData.cookies.AWSALBCORS,
+    );
+    pizzaId = makePizza(`${BASE_URL}/api/pizza`, requestConfig());
+  });
+  let URL = `${BASE_URL}/api/ratings`;
   group("Ratings CRUD operations", () => {
     let ratingId = createRating(URL, pizzaId, requestConfig());
     fetchRatings(URL, requestConfig());
